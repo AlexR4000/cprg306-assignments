c1dc839137f43e2fd097e69acb0fbcad69032afa
AlexR4000 <Alexander.Raagas@edu.sait.ca>
2025-10-06 13:06:14 -0600

    Added Nav Bar to app/layout.js as well cleaned out  app/page.js

--- app/page.js
+++ app/page.js
@@ -1,26 +1,9 @@
 // app/page.js
-
-import Link from "next/link";
-
 export default function Home() {
   return (
-    <main className="p-4">
-      <h1 className="text-2xl font-bold mb-4">
-        CPRG 306: Web Development 2 - Assignments
-      </h1>
-      <ul>
-        <li>
-        <Link href= "/week-2" className="text-cyan-600 underline hover:text-cyan-300">Go to Week 2 Page</Link>
-        </li>
-        <li>
-        <Link href= "/week-3" className="text-cyan-600 underline hover:text-cyan-300">Go to Week 3 Page</Link>
-        </li>
-        <li>
-        <Link href="/week-4" className="text-cyan-600 underline hover:text-cyan-300">Go to Week 4 Page</Link>
-        </li>
-      </ul>
-      
-      
-    </main>
+    <div>
+      <h1 className="text-3xl font-bold mb-4">Welcome to CPRG 306 Assignments</h1>
+      <p>Select a week from the sidebar to view the assignment page.</p>
+    </div>
   );
 }
